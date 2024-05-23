cf0ffb1beeec407f3b9b7024dae765a586b01627
Otabek1990 <fozilovotabek424@gmail.com>
2024-05-23 13:50:22 +0500

    fix/agent-detail-modified

--- src/pages/agent/AgentInformation.tsx
+++ src/pages/agent/AgentInformation.tsx
@@ -1,6 +1,7 @@
 import { EyeOutlined } from '@ant-design/icons';
 import { errorHandler } from '@config/axios_config';
-import { Button,  Col, Modal, Row } from 'antd';
+import { Button, Card, Col, Descriptions, Modal, Row, Typography } from 'antd';
+import { t } from 'i18next';
 import { useState } from 'react';
 import { AgentDetail, AgentService } from 'services/openapi';
 
@@ -15,17 +16,17 @@ const AgentInformation = ({ id }: Props) => {
     setOpen(true);
     try {
       let res = await AgentService.agentDetailNowRead(id);
+      console.log(res)
       setAgent(res);
     } catch (e: any) {
       errorHandler(e?.body);
     }
   };
 
-
   const handleClose = () => {
     setOpen(false);
   };
-
+console.log(agent)
   return (
     <>
       <Button type="dashed" size="middle" icon={<EyeOutlined className="mr-0" />} onClick={handleOpen} />
@@ -36,9 +37,23 @@ const AgentInformation = ({ id }: Props) => {
         title={'Agent haqida malumot'}
         width={1000}
       >
-        <Row>
-          <Col span={12}>{agent?.name}</Col>
-        </Row>
+        <Typography.Title level={3} style={{ marginBottom: '15px', textAlign: 'center' }}>
+          {t('Agent infos')}
+        </Typography.Title>
+        <Card style={{ width: '100%', borderRadius: 10, overflow: 'hidden' }}>
+          <Descriptions column={1} style={{ marginTop: 20 }}>
+            <Descriptions.Item label={t('Username')}>{agent?.username}</Descriptions.Item>
+            <Descriptions.Item label={t('Name')}>{agent?.name}</Descriptions.Item>
+            <Descriptions.Item label={t('Surname')}>{agent?.surname}</Descriptions.Item>
+            <Descriptions.Item label={t('Middlename')}>{agent?.middle_name}</Descriptions.Item>
+            <Descriptions.Item label={t('Birthday')}>{agent?.birthday}</Descriptions.Item>
+            <Descriptions.Item label={'Pasport'}>
+              {agent?.passport_seria} {agent?.passport_number}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('Work percentage')}>{agent?.percentage_of_work} %</Descriptions.Item>
+            <Descriptions.Item label="Address">{'address'}</Descriptions.Item>
+          </Descriptions>
+        </Card>
       </Modal>
     </>
   );
